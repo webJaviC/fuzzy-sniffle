3299cd53a33adb900cd3bcaa9de1640e5f125543
webJaviC <javicairoli3@gmail.com>
2023-06-25 18:24:06 -0300

    tercer commit
    tercer commit

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,52 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
+	window.addEventListener("resize", () =>{
+		
+		console.log("resized!");
+		const scaleX = window.innerWidth / app.screen.width; 
+		const scaleY = window.innerHeight / app.screen.height ;
+		const scale = Math.min(scaleX, scaleY);
 
+		const gameWidth = Math.round(app.screen.width * scale);
+		const gameHeight = Math.round(app.screen.height * scale);
+
+		const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+		const marginVertical = Math.floor((window.innerHeight - gameHeight)/2);
+
+		const view = app.view as HTMLCanvasElement;
+		view.style.width = gameWidth + "px";
+		view.style.height = gameHeight + "px";
+
+		view.style.marginLeft = marginHorizontal + "px";
+		view.style.marginRight = marginHorizontal + "px";
+
+		view.style.marginTop = marginVertical + "px";
+		view.style.marginBottom = marginVertical + "px";
+
+	});
+	window.dispatchEvent(new Event("resize"));
  Assets.add("Clampy","./clampy.png" );
-Assets.add("myDino","./dino.jpg" );
+Assets.add("myDino","./dino.png" );
 
 Assets.load(["Clampy","myDino"]).then(() => {
 
-const clampy: Sprite = Sprite.from("./dino.jpg");
+const dino: Sprite = Sprite.from("./dino.png");
 
-console.log("Hola mundo", clampy.width, clampy.height);
+console.log("Hola mundo", dino.width, dino.height);
 // clampy.anchor.set(0.5);
 
-clampy.x = 0;
-clampy.y = 0;
+dino.x = 0;
+dino.y = 0;
 
-app.stage.addChild(clampy);
+dino.scale.y = 0.5;
+dino.scale.x = 0.5;
+
+
+
+app.stage.addChild(dino);
 
 });
